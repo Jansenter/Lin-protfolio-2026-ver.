a1526ed8d90ecf63f951bdbfe9571f0dfc051dce
Jansenter <105319103+Jansenter@users.noreply.github.com>
2026-05-31 11:32:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//mainIndex slideshow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    
+    // Reset to first slide if at the end
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    
+    // Display the current slide
+    slides[slideIndex - 1].style.display = "block";  
+    
+    // Change image every 5 seconds
+    setTimeout(showSlides, 5000); 
+}
