25bcdd7d9f6f14359dc2cde0ee9105db70906ab4
“kelituol” <kelituol@students.cs.ubc.ca>
2026-05-31 15:33:35 -0700

    Added portfolio images second attempt

---
+++ images/Art-or-Algorithm/Jansenter Lin Li_files/runtime-env.js
@@ -0,0 +1,5 @@
+window.runtimeEnv = {
+  NEXT_PUBLIC_VORTIC_WEBAPP_URL: "https://vortic.art",
+  NEXT_PUBLIC_SENTRY_AUTH: "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",
+  NEXT_PUBLIC_API_URL: "https://api.vortic.art/",
+};
